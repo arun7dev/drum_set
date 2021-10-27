26746d197654d4cdad8759ccd99377f1ec4c0a5b
ARUN <arun042000@gmail.com>
2021-10-27 22:55:02 +0530

    drum kit

---
+++ index.js
@@ -0,0 +1,69 @@
+for (var i = 0; i < document.querySelectorAll("button").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener('click', handleClick);
+}
+
+function handleClick() {
+
+makeSound(this.innerHTML);
+animatedButton(this.innerHTML);
+
+}
+
+document.addEventListener("keydown",function(event){
+makeSound(event.key);
+animatedButton(event.key);
+
+})
+
+function makeSound(e){
+  switch (e) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+      case "a":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+        case "s":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+
+          case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+            case "j":
+              var tom2 = new Audio("sounds/tom-2.mp3");
+              tom2.play();
+              break;
+
+              case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+                case "l":
+                  var tom4 = new Audio("sounds/tom-4.mp3");
+                  tom4.play();
+                  break;
+    default: console.log(this.innerHTML);
+
+  }
+}
+
+
+function animatedButton(key){
+  var activeButton=document.querySelector("."+key);
+
+  activeButton.classList.add("pressed");
+  setTimeout(() => {    activeButton.classList.remove("pressed"); }, 100);
+
+
+
+
+}
